248ce2317269e9cd8c1fe1cf040dbd72dfb161ad
arne <arnemwinter@gmx.de>
2019-08-03 22:44:07 +0200

    health bars are now red

--- core/src/main/java/de/oul/gamejam/JamGame.java
+++ core/src/main/java/de/oul/gamejam/JamGame.java
@@ -1,16 +1,34 @@
 package de.oul.gamejam;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.kotcrab.vis.ui.VisUI;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class JamGame extends Game {
-	/** How many pixels make up a meter */
-	public static final float PIXELS_PER_METER = 32;
+  /** How many pixels make up a meter */
+  public static final float PIXELS_PER_METER = 32;
 
-	@Override
-	public void create() {
-		VisUI.load();
-		setScreen(new FirstScreen());
-	}
+  @Override
+  public void create(){
+    setUpUI();
+    setScreen(new FirstScreen());
+  }
+
+  private void setUpUI(){
+    VisUI.load();
+
+    TextureRegionDrawable defaultSkin = (TextureRegionDrawable) VisUI.getSkin().getDrawable("progressbar-filled");
+
+
+    Pixmap pixmap = new Pixmap(10, 10, Pixmap.Format.RGBA8888);
+    pixmap.setColor(0xFF0000FF);
+    pixmap.fill();
+    defaultSkin.setRegion(new TextureRegion(new Texture(pixmap)));
+
+    pixmap.dispose();
+  }
 }
