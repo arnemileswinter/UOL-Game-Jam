93302df4ca0b8dbf9e615ec739dd9a81c7adfb75
Manuel Wilde <MWildeclp@outlook.de>
2019-08-04 00:39:40 +0200

    Resolution Choose and bugfix (nullpointer after killing an enemy and got hit by his bullet)
    Merge branch 'master' of https://git.arnemileswinter.de/skalli/UOL-Game-Jam
    
    # Conflicts:
    #	core/src/main/java/de/oul/gamejam/system/RenderingSystem.java

--- lwjgl3/src/main/java/de/oul/gamejam/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/de/oul/gamejam/lwjgl3/Lwjgl3Launcher.java
@@ -3,11 +3,66 @@ package de.oul.gamejam.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.oul.gamejam.JamGame;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.control.ChoiceDialog;
 
-/** Launches the desktop (LWJGL3) application. */
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
 public class Lwjgl3Launcher {
+    private static int width;
+    private static int height;
+
     public static void main(String[] args) {
-        createApplication();
+        SwingUtilities.invokeLater(() -> {
+            new JFXPanel(); // this will prepare JavaFX toolkit and environment
+            Platform.runLater(() -> {
+
+                List<String> choices = new ArrayList<>();
+                choices.add("1600 x 900");
+                choices.add("1400 x 900");
+                choices.add("1366 x 768");
+                choices.add("1280 x 800");
+
+                ChoiceDialog<String> dialog = new ChoiceDialog<>("1600 x 900", choices);
+                dialog.setTitle("Ying and Jam");
+                dialog.setHeaderText("Welcome");
+                dialog.setContentText("Choose your Resolution:");
+
+// Traditional way to get the response value.
+                Optional<String> result = dialog.showAndWait();
+                if (result.isPresent()) {
+                    switch (result.get()) {
+                        case "1600 x 900":
+                            width = 1600;
+                            height = 900;
+                            break;
+                        case "1400 x 900":
+                            width = 1400;
+                            height = 900;
+                            break;
+                        case "1366 x 768":
+                            width = 1366;
+                            height = 768;
+                            break;
+                        case "1280 x 800":
+                            width = 1280;
+                            height = 800;
+                            break;
+                    }
+                    createApplication();
+                }
+
+// The Java 8 way to get the response value (with lambda expression)..add("1280 x 800");
+            });
+        });
+        //createApplication();
     }
 
     private static Lwjgl3Application createApplication() {
@@ -17,8 +72,9 @@ public class Lwjgl3Launcher {
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
         configuration.setTitle("OULGameJam");
-        configuration.setWindowedMode(640, 480);
+        configuration.setWindowedMode(width, height);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
+
 }
