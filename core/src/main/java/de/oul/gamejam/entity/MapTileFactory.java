358d99350cd6b7c7f5d44c6994699b413a17161c
arne <arnemwinter@gmx.de>
2019-08-03 14:29:39 +0200

    world logic no longer depends on the engine but rather on a mapTileFactory

--- core/src/main/java/de/oul/gamejam/entity/MapTileFactory.java
+++ core/src/main/java/de/oul/gamejam/entity/MapTileFactory.java
@@ -5,6 +5,8 @@ import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.*;
+import de.oul.gamejam.component.PhysicsComponent;
 import de.oul.gamejam.component.PositionComponent;
 import de.oul.gamejam.component.TextureComponent;
 
@@ -13,21 +15,52 @@ public class MapTileFactory {
 
     private final PooledEngine engine;
     private final TextureRegion textureRegion;
+    private final World world;
 
-    public MapTileFactory(PooledEngine engine){
+    public MapTileFactory(PooledEngine engine, World world){
         this.engine = engine;
+        this.world = world;
         textureRegion = new TextureRegion(new Texture(Gdx.files.internal("Wall.png")));
     }
 
-    public Entity createWand(float x, float y){
-        Entity title = engine.createEntity();
+    public Entity createWall(float x, float y){
+        Entity tile = engine.createEntity();
         PositionComponent position = engine.createComponent(PositionComponent.class);
         position.vector.set(x,y);
-        title.add(position);
+        tile.add(position);
         TextureComponent texture = engine.createComponent(TextureComponent.class);
         texture.textureRegion = textureRegion;
-        title.add(texture);
-        engine.addEntity(title);
-        return title;
+        tile.add(texture);
+
+        PhysicsComponent physicsComponent = engine.createComponent(PhysicsComponent.class);
+        physicsComponent.body = createWallPhysics(x, y);
+        tile.add(physicsComponent);
+
+        engine.addEntity(tile);
+        return tile;
+    }
+
+    /**
+     * Adds a physics body to the wall tile.
+     * @param x The x-coordinate of the wall
+     * @param y The y-coordinate of the wall.
+     * @return The physics body.
+     */
+    private Body createWallPhysics(float x, float y){
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(x,y);
+        Body body = world.createBody(bodyDef);
+
+        PolygonShape polygonShape = new PolygonShape();
+        polygonShape.setAsBox(0.5f,0.5f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = polygonShape;
+        body.createFixture(fixtureDef);
+
+        polygonShape.dispose();
+
+        return body;
     }
 }
--- core/src/main/java/de/oul/gamejam/world/LevelFactory.java
+++ core/src/main/java/de/oul/gamejam/world/LevelFactory.java
@@ -1,53 +1,54 @@
 package de.oul.gamejam.world;
 
 import com.badlogic.ashley.core.PooledEngine;
+import de.oul.gamejam.entity.MapTileFactory;
 
 import java.util.Random;
 
 public class LevelFactory {
-    PooledEngine engine;
-    private Room[][] rooms = new Room[10][10];
-    Random random = new Random();
+  private final MapTileFactory mapTileFactory;
+  Random random = new Random();
+  private Room[][] rooms = new Room[10][10];
 
-    public LevelFactory(PooledEngine pooledEngine) {
-        this.engine = pooledEngine;
-    }
+  public LevelFactory(MapTileFactory mapTileFactory){
+    this.mapTileFactory = mapTileFactory;
+  }
 
-    public void createLevel() {
-        for(int x = 0; x<10;x++) {
-            for (int y = 9; y >=0; y--) {
-                boolean bottomDoor = randomBoolean();
-                boolean leftDoor= randomBoolean();
-                boolean rightDoor = randomBoolean();
-                boolean topDoor = randomBoolean();
+  public void createLevel(){
+    for (int x = 0; x < 10; x++) {
+      for (int y = 9; y >= 0; y--) {
+        boolean bottomDoor = randomBoolean();
+        boolean leftDoor   = randomBoolean();
+        boolean rightDoor  = randomBoolean();
+        boolean topDoor    = randomBoolean();
 
-                if(x==0){
-                    leftDoor = false;
-                }else {
-                    leftDoor = rooms[y][x-1].rightDoor;
-                }
-                if(x==9){
-                    rightDoor = false;
-                }
-
-                if(y==9){
-                    bottomDoor = false;
-                }else{
-                    bottomDoor = rooms[y+1][x].topDoor;
-                }
-                if(!bottomDoor && !leftDoor && !rightDoor && !topDoor){
-                    topDoor = true;
-                }
-                if(y==0){
-                    topDoor = false;
-                }
-                rooms[y][x]=new BasicRoom(engine,x,y,bottomDoor,leftDoor,rightDoor,topDoor);
-            }
+        if (x == 0) {
+          leftDoor = false;
+        } else {
+          leftDoor = rooms[y][x - 1].rightDoor;
         }
-    }
+        if (x == 9) {
+          rightDoor = false;
+        }
 
-    private boolean randomBoolean(){
-        int number = random.nextInt(10);
-        return number <7;
+        if (y == 9) {
+          bottomDoor = false;
+        } else {
+          bottomDoor = rooms[y + 1][x].topDoor;
+        }
+        if (!bottomDoor && !leftDoor && !rightDoor && !topDoor) {
+          topDoor = true;
+        }
+        if (y == 0) {
+          topDoor = false;
+        }
+        rooms[y][x] = new BasicRoom(mapTileFactory, x, y, bottomDoor, leftDoor, rightDoor, topDoor);
+      }
     }
+  }
+
+  private boolean randomBoolean(){
+    int number = random.nextInt(10);
+    return number < 7;
+  }
 }
