5abbfd1f665822d72493108b3cda4f4389e2b672
arne <arnemwinter@gmx.de>
2019-08-03 14:33:31 +0200

    velocityComponent now contains the top speed

--- core/src/main/java/de/oul/gamejam/component/SpeedComponent.java
+++
@@ -1,5 +0,0 @@
-package de.oul.gamejam.component;
-
-public class SpeedComponent {
-  public float speed = 0.25f;
-}
--- core/src/main/java/de/oul/gamejam/component/VelocityComponent.java
+++ core/src/main/java/de/oul/gamejam/component/VelocityComponent.java
@@ -8,4 +8,6 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class VelocityComponent implements Component {
   public Vector2 vector;
+  // 0.25 tiles per second
+  public float speed = 0.25f;
 }
