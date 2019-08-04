2fd856b78c3a0b4733321d262c4b268ba10b2f1f
arne <arnemwinter@gmx.de>
2019-08-04 10:51:23 +0200

    textures are now cached

--- core/src/main/java/de/oul/gamejam/system/SwitchAssetSystem.java
+++ core/src/main/java/de/oul/gamejam/system/SwitchAssetSystem.java
@@ -6,15 +6,18 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ObjectMap;
 import de.oul.gamejam.component.SwitchAssetComponent;
 import de.oul.gamejam.component.TextureComponent;
 
 
 public class SwitchAssetSystem extends IteratingSystem {
     private float v = 0;
+    private final ObjectMap<String, Texture> textureCache;
 
     public SwitchAssetSystem() {
         super(Family.all(SwitchAssetComponent.class).get());
+        textureCache = new ObjectMap<>();
     }
 
     @Override
@@ -25,7 +28,12 @@ public class SwitchAssetSystem extends IteratingSystem {
             switchAssetComponent.switchAsset();
             TextureRegion textureRegion = entity.getComponent(TextureComponent.class).textureRegion;
             String assertString = switchAssetComponent.assetString;
-            textureRegion.setTexture(new Texture(Gdx.files.internal(assertString + switchAssetComponent.value + ".png")));
+            Texture texture = textureCache.get(assertString);
+            if(texture == null) {
+                texture = new Texture(Gdx.files.internal(assertString + switchAssetComponent.value + ".png"));
+                textureCache.put(assertString, texture);
+            }
+            textureRegion.setTexture(texture);
             this.v = 0f;
         }
     }
