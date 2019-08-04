0b316a7ccb9059d5aa9bebfd5f72b78962442704
arne <arnemwinter@gmx.de>
2019-08-04 10:58:33 +0200

    more texture caching

--- core/src/main/java/de/oul/gamejam/system/ViewSystem.java
+++ core/src/main/java/de/oul/gamejam/system/ViewSystem.java
@@ -7,38 +7,60 @@ import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ObjectMap;
 import de.oul.gamejam.View;
-import de.oul.gamejam.component.PlayerComponent;
 import de.oul.gamejam.component.PositionComponent;
 import de.oul.gamejam.component.TextureComponent;
 import de.oul.gamejam.component.ViewComponent;
 
 public class ViewSystem extends IteratingSystem {
-    ComponentMapper<TextureComponent> tm = ComponentMapper.getFor(TextureComponent.class);
-    ComponentMapper<ViewComponent> vm = ComponentMapper.getFor(ViewComponent.class);
+  private final ObjectMap<String, Texture> textureCache = new ObjectMap<>();
+  ComponentMapper<TextureComponent> tm = ComponentMapper.getFor(TextureComponent.class);
+  ComponentMapper<ViewComponent>    vm = ComponentMapper.getFor(ViewComponent.class);
 
-    public ViewSystem() {
-        super(Family.all(PositionComponent.class,ViewComponent.class,TextureComponent.class).get());
-    }
+  public ViewSystem(){
+    super(Family.all(PositionComponent.class, ViewComponent.class, TextureComponent.class).get());
+  }
 
-    @Override
-    protected void processEntity(Entity entity, float v) {
-        View view = vm.get(entity).view;
-        TextureRegion textureRegion = tm.get(entity).textureRegion;
-        String assetString = entity.getComponent(ViewComponent.class).assetString;
-        switch (view){
-            case Up:
-                textureRegion.setTexture(new Texture(Gdx.files.internal(assetString+"Up.png")));
-                break;
-            case Left:
-                textureRegion.setTexture(new Texture(Gdx.files.internal(assetString+"Left.png")));
-                break;
-            case Down:
-                textureRegion.setTexture(new Texture(Gdx.files.internal(assetString+"Down.png")));
-                break;
-            case Right:
-                textureRegion.setTexture(new Texture(Gdx.files.internal(assetString+"Right.png")));
-                break;
+  @Override
+  protected void processEntity(Entity entity, float v){
+    View          view          = vm.get(entity).view;
+    TextureRegion textureRegion = tm.get(entity).textureRegion;
+    String        assetString   = entity.getComponent(ViewComponent.class).assetString;
+    Texture       cached        = null;
+    switch (view) {
+      case Up:
+        cached = textureCache.get(assetString + "Up");
+        if (cached == null) {
+          cached = new Texture(Gdx.files.internal(assetString + "Up.png"));
+          textureCache.put(assetString + "Up", cached);
         }
+        textureRegion.setTexture(cached);
+        break;
+      case Left:
+        cached = textureCache.get(assetString + "Left");
+        if (cached == null) {
+          cached = new Texture(Gdx.files.internal(assetString + "Left.png"));
+          textureCache.put(assetString + "Left", cached);
+        }
+        textureRegion.setTexture(cached);
+        break;
+      case Down:
+        cached = textureCache.get(assetString + "Down");
+        if (cached == null) {
+          cached = new Texture(Gdx.files.internal(assetString + "Down.png"));
+          textureCache.put(assetString + "Down", cached);
+        }
+        textureRegion.setTexture(cached);
+        break;
+      case Right:
+        cached = textureCache.get(assetString + "Right");
+        if (cached == null) {
+          cached = new Texture(Gdx.files.internal(assetString + "Right.png"));
+          textureCache.put(assetString + "Right", cached);
+        }
+        textureRegion.setTexture(cached);
+        break;
     }
+  }
 }
